93a5af1f01ceb7285f11f2b493c8c4515cf58fee
David Hin <hello@davidhin.com>
2021-06-07 13:15:05 +0930

    Update filtering

--- vuldataexplore/analysis/main.py
+++ vuldataexplore/analysis/main.py
@@ -1,6 +1,5 @@
 # %%
 import xml.etree.ElementTree as ET
-from collections import Counter
 
 import pandas as pd
 import vuldataexplore as vde
@@ -12,15 +11,20 @@ root = tree.getroot()
 # %%
 test_id_flaws = []
 for child in tqdm(root):
+    status = child.attrib["status"]
     testid = child.attrib["id"]
     lang = child.attrib["language"]
-    num_files = child.attrib["numberOfFiles"]
+
+    # FILTER: Deprecated
+    if status == "Deprecated":
+        continue
 
     # FILTER: Programming language
     if lang not in ["C", "C++"]:
         continue
 
-    suslines = []
+    # Extract list of flawed/mixed lines.
+    markedlines = []
     for testcasefile in child.findall("./file"):
         flaws = []
         for fileline in testcasefile:
@@ -34,20 +38,36 @@ for child in tqdm(root):
         # FILTER: flawed/mixed lines with line_number == 0
         flaws = [i for i in flaws if i["line"] != "0"]
         if len(flaws) > 0:
-            suslines += flaws
+            markedlines += flaws
 
     # FILTER: At least one flawed/mixed line
-    if len(suslines) == 0:
+    if len(markedlines) == 0:
+        continue
+
+    # FILTER: No fixlines (good cases)
+    num_fixlines = len([i for i in markedlines if i["linetag"] == "fix"])
+    if num_fixlines > 0:
         continue
 
     test_id_flaws.append(
         {
             "testid": testid,
-            "files": suslines,
-            "num_suslines": len(suslines),
+            "files": markedlines,
+            "num_markedlines": len(markedlines),
+            "num_flawlines": len([i for i in markedlines if i["linetag"] == "flaw"]),
+            "num_mixedlines": len([i for i in markedlines if i["linetag"] == "mixed"]),
+            "num_fixlines": num_fixlines,
             "lang": lang,
-            "num_files": num_files,
-            "cwes": [i["name"] for i in suslines],
+            "num_files": len(child.findall("./file")),
+            "cwes": [i["name"] for i in markedlines],
+            "status": status,
         }
     )
 df = pd.DataFrame.from_records(test_id_flaws)
+
+df[df.testid == "91116"]
+
+# %% Statistics to check
+# How spread apart the individual lines are vs hunks
+# How long each of the files are
+# Distribution of number of files
