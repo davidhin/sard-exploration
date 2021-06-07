7cc971bc7eca992b3fc7e98af4c2c0f9c7dda020
David Hin <hello@davidhin.com>
2021-06-07 13:31:38 +0930

    Restructure scripts

---
+++ vuldataexplore/analysis/plots.py
@@ -0,0 +1,10 @@
+# %%
+import vuldataexplore as vde
+import vuldataexplore.helpers as vdeh
+
+df = vdeh.get_sard_df()
+
+# %% Statistics to check
+# How spread apart the individual lines are vs hunks
+# How long each of the files are
+# Distribution of number of files
